ebfae87b66e495c83112cb0480df910a645e13a0
Joan Killeen <joan.killeen@bluewin.ch>
2021-12-20 17:17:20 +0100

    first pytest

--- song_test.py
+++ song_test.py
@@ -1,4 +1,14 @@
 import song as Song
-def test_output():
+import pytest
+REFERENCE_SONG_FILEPATH = "testdata/reference_song.txt"
+
+@pytest.fixture
+def reference_song():
+    f = open(REFERENCE_SONG_FILEPATH, "r")
+    return f.read()
+
+def test_output(reference_song):
     output = Song.output()
-    print(output)
+    assert output == reference_song
+    print(reference_song)
+    
